a97c504f3f18f56d3e1f9b8eb169ff36e58f5e49
Virender Singh <dk77089@gmail.com>
2026-06-10 13:59:51 +0530

    designing_day_2

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.header />
+    <main>
+        {{ $slot }}
+    </main>
+<x-layouts.footer />
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+})->name('aboutus');
+
+Route::get('/bloglist', function () {
+    return view('bloglist');
+})->name('bloglist');
+
+Route::get('/singleblog', function () {
+    return view('singleblog');
+})->name('singleblog');
